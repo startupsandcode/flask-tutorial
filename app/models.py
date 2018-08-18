4ac1cf61961532e939c2b0c48cf836b7f4bf93e0
lotekmedia <me@johnmann.org>
2018-08-18 09:11:54 -0400

    updated for followers

--- app/models.py
+++ app/models.py
@@ -4,6 +4,11 @@ from app import db, login
 from flask_login import UserMixin
 from hashlib import md5
 
+followers = db.Table('followers', 
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -13,6 +18,12 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -38,3 +49,4 @@ class Post(db.Model):
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
