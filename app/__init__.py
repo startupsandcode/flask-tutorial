9c342d4ee0356526606a96cc1e7231311c71cfa7
lotekmedia <me@johnmann.org>
2018-08-19 11:30:58 -0400

    deploy

--- app/__init__.py
+++ app/__init__.py
@@ -24,7 +24,8 @@ bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
-if not app.debug:
+
+if not app.debug and not app.testing:
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
@@ -40,15 +41,20 @@ if not app.debug:
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
 
-        if not os.path.exists('logs'):
-            os.mkdir('logs')
-        
-        file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
-                                        backupCount=10)
-        file_handler.setFormatter(logging.Formatter(
-            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            
+            file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                            backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
 
         app.logger.setLevel(logging.INFO)
         app.logger.info('Microblog startup')
--- config.py
+++ config.py
@@ -15,3 +15,5 @@ class Config(object):
     ADMINS = ['your-email@example.com']
 
     POSTS_PER_PAGE = 3
+
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
