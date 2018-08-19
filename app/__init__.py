29de7db5f9e6410113c7319d98fb0fdce880a5a3
lotekmedia <me@johnmann.org>
2018-08-19 10:46:53 -0400

    added bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
@@ -18,6 +19,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 
 mail = Mail(app)
+
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
