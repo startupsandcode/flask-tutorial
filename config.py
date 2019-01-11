35f143e7780c3728f660290d0678b534cc62ee29
John Mann <jmann@consumer-edge.com>
2019-01-11 09:23:50 -0500

    updated logo

--- config.py
+++ config.py
@@ -5,18 +5,18 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+		'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+	MAIL_SERVER = os.environ.get('MAIL_SERVER')
+	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+	ADMINS = ['your-email@example.com']
 
-    POSTS_PER_PAGE = 3
+	POSTS_PER_PAGE = 3
 
-    USERS_PER_PAGE = 3
+	USERS_PER_PAGE = 3
